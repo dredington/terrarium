a9098a9125d1a46edd5d12577ef59b73c6a9a87d
Daniel Redington <dan@getnotion.com>
2017-11-25 00:46:22 -0700

    Simple unit test

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='terrarium',
+   version='0.0.1',
+   description='IoT Hub for monitoring Hydroponics',
+   author='dredington',
+   author_email='',
+   packages=['terrarium'],
+   install_requires=[]
+)
---
+++ terrarium/temperature_report.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+class TemperatureReport:
+
+  def hello(self):
+    print("Hello?")
+    return True
