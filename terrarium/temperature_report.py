83e0e8c06898ba28f755d5c8cf509a654d402fc8
Daniel Redington <dan@getnotion.com>
2018-07-11 23:06:14 -0600

    Improved temperature_report tests

--- terrarium/temperature_report.py
+++ terrarium/temperature_report.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from weather import Weather, Unit
 
+
 class TemperatureReport:
 
   def __init__(self):
--- test/test_temperature_report.py
+++ test/test_temperature_report.py
@@ -5,17 +5,22 @@ from weather.objects.weather_obj import *
 
 import unittest
 
+
 class TestTemperatureReport(unittest.TestCase):
 
   def setUp(self):
       self.report = TemperatureReport()
 
   def test_temperature(self):
+    """Is temperature an integer?"""
     self.assertTrue(type(self.report.temperature()) is int)
 
   def test_humidity(self):
-    self.assertTrue(type(self.report.humidity()) is int)
-    self.assertTrue(0 <= self.report.humidity() <= 100)
+    """Is humidity an integer and a valid percentage?"""
+    humidity = self.report.humidity()
+    self.assertTrue(type(humidity) is int)
+    self.assertTrue(0 <= humidity <= 100)
+
 
 if __name__ == '__main__':
     unittest.main()
