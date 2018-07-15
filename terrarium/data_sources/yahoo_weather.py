691d4c4eca24df6ead69bd76badce30161a43050
Daniel Redington <dan@getnotion.com>
2018-07-15 15:59:12 -0600

    Update launch script to use new architecture
    - YahooWeather is a DataSource

--- terrarium/data_sources/yahoo_weather.py
+++ terrarium/data_sources/yahoo_weather.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 from weather import Weather, Unit
 
-class YahooWeather:
+from sentinel import DataSource
+
+class YahooWeather(DataSource):
 
   def __init__(self):
     self.weather = Weather(unit=Unit.FAHRENHEIT)
     self.lookup = self.weather.lookup_by_location(80031)
     self.condition = self.lookup.condition
+
+  def report(self):
+    return { 'temperature': self.temperature(), 'humidity': self.humidity() }
 
   def temperature(self):
     return int(self.condition.temp)
